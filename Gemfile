956ba61a0b93212bd815fe5ff41c4f347ee30ea7
Seliverstov Maxim <maxim.web.developer@gmail.com>
2014-09-19 23:27:20 +0000

    Add gems for testing. Init test_helper.

--- Gemfile
+++ Gemfile
@@ -19,12 +19,16 @@ gem 'haml-rails'
 gem 'twitter-bootstrap-rails'
 
 gem 'ransack', github: 'activerecord-hackery/ransack', branch: 'rails-4.1'
+#временно, пока не поправят баг в основной репе (state-machine)
 gem 'state_machine', github: 'seuros/state_machine'
 
 group :development, :test do
   #gem 'spring'
+  gem 'factory_girl'
+  gem 'minitest-rails'
   gem 'capistrano-rails'
   gem 'debugger'
   gem 'pry'
+  gem 'wrong'
 end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,13 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] = "test"
+
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
+require "minitest/rails"
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  ActiveRecord::Migration.check_pending!
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  include FactoryGirl::Syntax::Methods
+  include Wrong
 end
